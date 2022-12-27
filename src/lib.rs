a5ee552e6b3d9a302bad6469f4570c274feeb3b7
kristopherbullinger <kris.bullinger@gmx.com>
2022-12-26 20:21:35 -0500

    change meaning of `count` field

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use rand::{rngs::ThreadRng, RngCore};
 use slotmap::{Key as _, SlotMap};
 use std::cmp::Ordering;
+use std::ops::Add;
 slotmap::new_key_type! {
     pub struct TreapKey;
 }
@@ -23,12 +24,9 @@ impl<T> TreapNode<T> {
             left: TreapKey::null(),
             right: TreapKey::null(),
             parent: TreapKey::null(),
-            count: 1, // self
+            // how many nodes are beneath this one in the tree -- NOT its ordinal position
+            count: 0,
         }
-    }
-
-    fn into_inner(self) -> T {
-        self.value
     }
 }
 
@@ -40,24 +38,48 @@ pub struct TreapList<T, RNG: RngCore = ThreadRng> {
 }
 
 impl<T, RNG: RngCore> TreapList<T, RNG> {
+    pub fn new() -> Self
+    where
+        T: Default,
+        RNG: Default,
+    {
+        Default::default()
+    }
+
     pub fn len(&self) -> usize {
         self.nodes.len()
     }
 
-    fn nth(&self, n: usize) -> Option<TreapKey> {
-        if n >= self.len() {
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn clear(&mut self) {
+        self.nodes.clear();
+        self.root = TreapKey::null();
+    }
+
+    fn nth(&self, mut n: usize) -> Option<TreapKey> {
+        if n > self.len() {
             return None;
         }
-        let n = n + 1;
         let mut cur = self.root;
         loop {
-            let TreapNode {
-                left, right, count, ..
-            } = self.nodes[cur];
-            match count.cmp(&n) {
-                Ordering::Greater => cur = left,
+            let node = self.nodes.get(cur)?;
+            let left_count = match self.nodes.get(node.left) {
+                Some(lnode) => lnode.count + 1,
+                None => 0,
+            };
+            match n.cmp(&left_count) {
+                Ordering::Greater => {
+                    n -= left_count;
+                    n -= 1; //current node
+                    cur = node.right;
+                }
                 Ordering::Equal => return Some(cur),
-                Ordering::Less => cur = right,
+                Ordering::Less => {
+                    cur = node.left;
+                }
             }
         }
     }
@@ -70,55 +92,50 @@ impl<T, RNG: RngCore> TreapList<T, RNG> {
         self.nth(n).map(|key| &mut self.nodes[key].value)
     }
 
-    pub fn insert(&mut self, value: T, pos: usize) -> Option<TreapKey> {
-        if self.len() < pos {
-            return None;
-        }
-        let node = TreapNode::new(value, self.rng.next_u64());
+    fn insert_node(&mut self, node: TreapNode<T>, pos: usize) -> Option<TreapKey> {
         let key = self.nodes.insert(node);
         let (left, right) = self.split(self.root, pos);
         let left = self.merge(left, key);
         self.root = self.merge(left, right);
         self.nodes[self.root].parent = TreapKey::null();
+        self.update(key);
         Some(key)
+    }
+
+    pub fn insert(&mut self, value: T, pos: usize) -> Option<TreapKey> {
+        if self.len() < pos {
+            return None;
+        }
+        let node = TreapNode::new(value, self.rng.next_u64());
+        self.insert_node(node, pos)
     }
 
     pub fn push(&mut self, value: T) -> TreapKey {
         self.insert(value, self.len()).unwrap()
     }
 
+    pub fn pop(&mut self) -> Option<T> {
+        self.remove(self.len())
+    }
+
     fn count(&self, key: TreapKey) -> usize {
         self.nodes.get(key).map(|node| node.count).unwrap_or(0)
     }
 
-    // fn position_of(&self, key: TreapKey) -> Option<usize> {
-    //     let node = self.nodes.get(key)?;
-    //     let mut pos = self.count(node.left);
-    //     let mut prev = key;
-    //     let mut cur = node.parent;
-    //     while let Some(parent) = self.nodes.get(cur) {
-    //         if parent.right == prev {
-    //             pos += self.count(parent.left) + 1;
-    //         }
-    //         prev = cur;
-    //         cur = parent.parent;
-    //     }
-    //     Some(pos)
-    // }
-
+    // point the node's children to this one, and update the count of the current node
     fn update(&mut self, key: TreapKey) {
         let (left, right) = match self.nodes.get(key) {
             Some(n) => (n.left, n.right),
             None => return,
         };
-        let mut count = 1;
+        let mut count = 0;
         if let Some(node) = self.nodes.get_mut(left) {
             node.parent = key;
-            count += node.count;
+            count += node.count + 1;
         }
         if let Some(node) = self.nodes.get_mut(right) {
             node.parent = key;
-            count += node.count;
+            count += node.count + 1;
         }
         self.nodes[key].count = count;
     }
@@ -155,7 +172,7 @@ impl<T, RNG: RngCore> TreapList<T, RNG> {
             self.update(node);
             (ll, node)
         } else {
-            let (rl, rr) = self.split(right, nth - left_count - 1);
+            let (rl, rr) = self.split(right, nth - left_count);
             self.nodes[node].right = rl;
             self.update(node);
             (node, rr)
@@ -163,25 +180,34 @@ impl<T, RNG: RngCore> TreapList<T, RNG> {
     }
 
     pub fn remove(&mut self, n: usize) -> Option<T> {
-        None
+        let key = self.nth(n)?;
+        self.remove_node(key).map(|node| node.value)
     }
 
-    pub fn remove_node(&mut self, key: TreapKey) -> Option<T> {
+    fn remove_node(&mut self, key: TreapKey) -> Option<TreapNode<T>> {
         let node = self.nodes.remove(key)?;
+
+        //update parent nodes' counts
+        let mut cur = node.parent;
+        while let Some(node) = self.nodes.get_mut(cur) {
+            node.count -= 1;
+            cur = node.parent;
+        }
+
         let merged = self.merge(node.left, node.right);
-        if let Some(parent) = self.nodes.get_mut(node.parent) {
-            if parent.right == key {
-                parent.right = merged;
+        if let Some(parent_node) = self.nodes.get_mut(node.parent) {
+            if parent_node.right == key {
+                parent_node.right = merged;
             } else {
-                parent.left = merged;
+                parent_node.left = merged;
             }
             self.update(node.parent);
         }
         if key == self.root {
             self.root = merged;
-            self.nodes[merged].parent = TreapKey::null();
         }
-        None
+
+        Some(node)
     }
 }
 
@@ -190,8 +216,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn insert() {
+        let mut tpl = TreapList::<u32>::new();
+        for n in 0..10 {
+            tpl.push(n);
+        }
+        for n in 0..10 {
+            assert_eq!(tpl.get(n).copied(), Some(n as u32));
+        }
+        let five = tpl.remove(5);
+        assert_eq!(five, Some(5));
+        for n in 0..9 {
+            let expected = if n >= 5 { n + 1 } else { n };
+            assert_eq!(tpl.get(n).copied(), Some(expected as u32), "{:#?}", tpl);
+        }
+    }
+
+    #[test]
+    fn remove_all() {
+        let mut tpl = TreapList::<u32>::new();
+        for n in 0..10 {
+            tpl.push(n);
+        }
+        for _ in 0..100 {
+            tpl.pop();
+        }
     }
 }
